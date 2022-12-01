b5f4327e8ea523ccbc6faa924cb3ea12aa27b335
Gudkova Mariia <gudmary8@gmail.com>
2022-12-01 20:50:36 +0400

    add models user and movie

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: (value) => {
+      if (validator.isEmail(value)) {
+        return true;
+      }
+      return false;
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+}, {
+  versionKey: false,
+});
+
+module.exports = mongoose.model('user', userSchema);
