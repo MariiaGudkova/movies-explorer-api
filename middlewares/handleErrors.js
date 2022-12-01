d917c8171e076e73ed13a44ea1ba157062d18e80
Gudkova Mariia <gudmary8@gmail.com>
2022-12-01 21:40:55 +0400

    add error handle

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -3,7 +3,7 @@ const { SERVER_ERROR_CODE } = require('../utils/constants');
 module.exports = (err, req, res, next) => {
   const { statusCode = SERVER_ERROR_CODE, message: errorMessage } = err;
   const message = statusCode === SERVER_ERROR_CODE
-    ? 'An error occurred on the server'
+    ? 'Server error'
     : errorMessage;
   res
     .status(statusCode)
