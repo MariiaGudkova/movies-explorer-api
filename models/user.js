2654f16838aeec9f561c409427661400d70de7d2
Gudkova Mariia <gudmary8@gmail.com>
2022-12-03 14:43:47 +0400

    add a user search method to the schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/unauthorized_err');
+const { AUTHORIZATION_ERROR_TEXT } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -27,5 +30,14 @@ const userSchema = new mongoose.Schema({
 }, {
   versionKey: false,
 });
+
+userSchema.statics.findUserByCredentials = async function findUserByCredentials(email, password) {
+  const user = await this.findOne({ email }).select('+password').orFail(() => new UnauthorizedError(AUTHORIZATION_ERROR_TEXT));
+  const matched = await bcrypt.compare(password, user.password);
+  if (!matched) {
+    return Promise.reject(new UnauthorizedError(AUTHORIZATION_ERROR_TEXT));
+  }
+  return user;
+};
 
 module.exports = mongoose.model('user', userSchema);
